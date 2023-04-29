cc01a1646e248e4ac1d107dbf9d72a2959ad2219
diazgonza17 <diazgonzalo1720@gmail.com>
2023-04-29 01:01:53 -0300

    Fully functional footer

--- portfolioFront/src/app/components/footer/footer.component.ts
+++ portfolioFront/src/app/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Persona } from 'src/app/models/persona';
 import { PersonaService } from 'src/app/services/persona.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,15 +9,57 @@ import { PersonaService } from 'src/app/services/persona.service';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  /*
-  contactData:any;
-  constructor(private datosPortfolio:PersonaService) {
+
+  constructor(private sPersona: PersonaService, private tokenService: TokenService) { }
+
+  perso: Persona;
+
+  cargarPersona(): void {
+    this.sPersona.detail().subscribe(data => {this.perso = data});
   }
 
+  isLogged: boolean = false;
+
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      this.contactData = data.contact;
-    });
+    this.cargarPersona();
+    if(this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
   }
-  */
+
+  email: string = '';
+
+  isEditing: boolean = false;
+
+  onEdit(event: MouseEvent): void {
+    const button = event.target as HTMLButtonElement;
+    const grandpa = button.closest(".container-fluid") as HTMLElement;
+    const content = grandpa.querySelector(".content") as HTMLElement;
+    const editcontent = grandpa.querySelector(".edit-content") as HTMLElement;
+    const buttons = grandpa.querySelector(".edit-delete-icons") as HTMLElement;
+    if(!this.isEditing){
+      content.style.display = 'none';
+      buttons.style.display = 'none';
+      editcontent.style.display = 'block';
+      this.isEditing = true;
+    }
+  }
+
+  onCloseEdit(): void {
+    const message = "Are you sure? All changes will be lost.";
+    const result = window.confirm(message);
+    window.location.reload();
+  }
+
+  onUpdate(): void {
+    this.sPersona.update(this.perso).subscribe(data => {
+      alert("Email modificado");
+      window.location.reload()
+    }, err => {
+      alert("Error al modificar email");
+      window.location.reload();
+    })
+  }
 }
